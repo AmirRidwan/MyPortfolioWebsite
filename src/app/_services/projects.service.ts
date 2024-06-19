c66ff3770735106cfbc9829fb08802d78a269ca6
AmirRidwan <121612926+AmirRidwan@users.noreply.github.com>
2024-06-19 11:04:17 +0800

    Update projects.service.ts

--- src/app/_services/projects.service.ts
+++ src/app/_services/projects.service.ts
@@ -14,7 +14,7 @@ export class ProjectsService {
       summary: 'MyEV is a mobile application designed to help electric vehicle (EV) drivers find charging stations.',
       description: 'MyEV is a mobile application designed to help electric vehicle (EV) drivers find charging stations. With EVs increasing popularity, it is becoming more important to have easy access to charging facilities. MyEV aims to make this process simple and convenient by providing a user-friendly interface that allows drivers to locate nearby charging stations quickly.',
       projectLink: '//www.github.com/AmirRidwan/MyEV-App',
-      pictures: ["../../assets/MyEV-1.png","../../assets/MyEV-2.png","../../assets/MyEV-3.png"],
+      pictures: ["assets/myev_1.png","assets/myev_2.png","assets/myev_3.png"],
       tags: [Tag.DART, Tag.FLUTTER]
     },
     {
@@ -23,7 +23,7 @@ export class ProjectsService {
       summary: 'Simple task manager app developed using Kotlin and Firebase.',
       description: 'MyTasks is a simple task manager app that allows users to add, edit, and delete tasks. It features real-time synchronization with a Firestore database, ensuring that tasks are always up to date. The app also includes a profile view and edit functionality for a personalized experience.', 
       projectLink: 'https://github.com/AmirRidwan/MyTasks',
-      pictures: ["../../assets/Image 1.png","../../assets/Image 2.png","../../assets/Image 3.png"],   
+      pictures: ["assets/image_1.png","assets/image_2.png","assets/image_3.png"],   
       tags: [Tag.KOTLIN] 
     },
     {
@@ -32,7 +32,7 @@ export class ProjectsService {
       summary: 'Recipe app is a simple recipe app that allows users to add recipes, view recipes, and save recipes.',
       description: 'Recipe app is a simple recipe app that allows users to add recipes, view recipes, and save recipes. The app features a user-friendly interface that makes it easy to navigate and find recipes. Users can add new recipes, view existing recipes, and save their favorite recipes for later.',
       projectLink: '//www.github.com/AmirRidwan/Recipe-App',
-      pictures: ["../../assets/Image 1.png","../../assets/Image 2.png","../../assets/Image 3.png"],
+      pictures: ["assets/image_1.png","assets/image_2.png","assets/image_3.png"],
       tags: [Tag.KOTLIN]
     },
     {
@@ -41,7 +41,7 @@ export class ProjectsService {
       summary: 'Clinic Finder App is a mobile application designed to help users find nearby clinics and hospitals.',
       description: 'Clinic Finder App is a mobile application designed to help users find nearby clinics and hospitals. The app features a user-friendly interface that allows users to search for clinics based on their location. Users can view clinic details, such as address, contact information, and services offered. The app also includes a map view that displays the location of nearby clinics.', 
       projectLink: '//www.github.com/AmirRidwan/ClinicFinderApps',
-      pictures: ["../../assets/Image 1.png","../../assets/Image 2.png","../../assets/Image 3.png"],
+      pictures: ["assets/image_1.png","assets/image_2.png","assets/image_3.png"],
       tags: [Tag.JAVA]
     }
   ]
